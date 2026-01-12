15f6621b6f85a1fde07356d27be7b3d1c1d707eb
mael135 <matanel135@gmail.com>
2026-01-12 17:34:47 +0200

    updated preproccess

--- src/data/preprocessData.py
+++ src/data/preprocessData.py
@@ -1,13 +1,19 @@
-import torch
-import torchaudio
-import torchaudio.transforms as transforms
-import pandas as pd
+# import torch
+# import torchaudio
+# import torchaudio.transforms as transforms
+# import pandas as pd
 from pathlib import Path
 
-from src.features.transforms import SpectrogramTransform
-from src.config import load_and_merge_configs
+# from src.features.transforms import SpectrogramTransform
+# from src.config import load_and_merge_configs
 
-config = load_and_merge_configs("config/default.yaml")
+# config = load_and_merge_configs("config/default.yaml")
 
-spectrogramTransform = SpectrogramTransform(config)
+# spectrogramTransform = SpectrogramTransform(config)
 
+def parse_filename_metadata(filename: str):
+    parts = Path(filename).stem.split('__')
+    return parts
+
+
+print(parse_filename_metadata("data/raw/2025-12-15__room__background__ambience__g50__22k05__5m__01.wav"))
